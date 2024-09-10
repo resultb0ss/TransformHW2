a124edef1e9180e19ba3c482f4ebf8fc23c8df98
Admin <reteamboss@gmail.com>
2024-09-10 19:37:58 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
-    println("Hello World!")
+    var argument = 2
+    println(checkArg(argument))
+
+}
+
+fun<T> checkArg(argument: T): String {
+    var result = ""
+    when(argument){
+        is String ->result = "Это строка"
+        is Int ->result = "Это число целое"
+        is Double ->result = "Это число с плавающей точкой"
+        is Char -> result = "Это символ"
+        else -> result ="Не знаю что это"
+
+    }
+    return result
 }
